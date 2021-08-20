0a5f9a3499ff4156faa7a996393f83097b2af957
adarke <47354567+adarke@users.noreply.github.com>
2021-08-21 00:02:40 +0100

    5-5: exercise complete

---
+++ chapter5/5-5/5-5.py
@@ -0,0 +1,22 @@
+""" 5-5 -- Favourite Songs. 
+
+Create a dictionary mapping your favorite musicians to a list of your favourite 
+songs by them.
+"""
+
+
+musicians = {"Phish": ["Squirming Coil",
+                       "Harry Hood",
+                       "Tela"],
+             "Grateful Dead": ["Jack Straw",
+                               "Wharf Rat",
+                               "Crazy Fingers"],
+             "Paul Simon": ["Crazy Love",
+                            "You Can Call Me Al",
+                            "Diamonds On The Soles Of Her Shoes"]
+             }
+
+print(musicians)
+print(musicians["Phish"])
+print(musicians["Grateful Dead"])
+print(musicians["Paul Simon"])
