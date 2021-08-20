1ee98393a2fb959b336dab5e2e3cb7c7599124c2
adarke <47354567+adarke@users.noreply.github.com>
2021-08-20 23:57:57 +0100

    4-2: exercise complete

---
+++ chapter4/4-2/4-2.py
@@ -0,0 +1,19 @@
+""" 4-2 -- Print String.
+
+Create a function that accepts a string as a parameter and prints it.
+"""
+
+
+def alt_print(x):
+    """ Alternative print function.
+
+    Args:
+        x: The string to print.
+
+    Returns:
+        None.
+    """
+    print(x)
+
+
+alt_print("test string")
