5315e9dc8b6f116ffff3ea8f5622d86cf316e448
adarke <47354567+adarke@users.noreply.github.com>
2021-08-21 00:06:07 +0100

    6-9: exercise complete

---
+++ chapter6/6-9/6-9.py
@@ -0,0 +1,9 @@
+""" 6-9 -- Concatenation And Multiplication.
+
+Create the string "three three three" using concatenation, and then again using 
+multiplication.
+"""
+
+
+print("three " + "three " + "three")
+print("three " * 3)
