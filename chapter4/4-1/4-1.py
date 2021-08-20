ab64953fd67dc29a3f3674077692d7efbf911036
adarke <47354567+adarke@users.noreply.github.com>
2021-08-20 23:57:45 +0100

    4-1: exercise complete

---
+++ chapter4/4-1/4-1.py
@@ -0,0 +1,20 @@
+""" 4-1 -- Square.
+
+Write a function that takes a number as an input and returns that number 
+squared.
+"""
+
+
+def square(x):
+    """ Calculate the square of the argument.
+
+    Args:
+        x: The number to be squared.
+
+    Returns:
+        The value of x squared.
+    """
+    return x * x
+
+
+print(square(3))
