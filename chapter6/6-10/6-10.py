3f0b4657ef79adace592f710665f868ff5899289
adarke <47354567+adarke@users.noreply.github.com>
2021-08-21 00:06:14 +0100

    6-10: exercise complete

---
+++ chapter6/6-10/6-10.py
@@ -0,0 +1,10 @@
+""" 6-10 -- Slice String.
+
+Slice the string "It was a bright cold day in April, and the clocks were 
+striking thirteen." to only include the characters before the comma.
+"""
+
+
+s = "It was a bright cold day in April, and the clocks were striking thirteen."
+
+print(s[:33])
