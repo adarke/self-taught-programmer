138fae4023e14f44333459add453628b98ea95c8
adarke <47354567+adarke@users.noreply.github.com>
2021-08-20 23:58:50 +0100

    5-1: exercise complete

---
+++ chapter5/5-1/5-1.py
@@ -0,0 +1,14 @@
+""" 5-1 -- Musicians List.
+
+Create a list of your favourite musicians.
+"""
+
+
+musicians = [
+    "Phish",
+    "Michael Hedges",
+    "TOOL",
+    "Franz Schubert",
+]
+
+print(musicians)
