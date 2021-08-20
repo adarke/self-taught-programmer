cc92362d3b40bd50ec50477c299ac84b7b3191ea
adarke <47354567+adarke@users.noreply.github.com>
2021-08-21 00:06:51 +0100

    7-3: exercise complete

---
+++ chapter7/7-3/7-3.py
@@ -0,0 +1,14 @@
+""" 7-3 -- Print Item And Index.
+
+Print each item in the list from the first challenge and their indexes.
+"""
+
+
+shows = ["The Walking Dead",
+         "Entourage",
+         "The Sopranos",
+         "The Vampire Diaries"]
+
+for i in shows:
+    print(i)
+    print(shows.index(i))
