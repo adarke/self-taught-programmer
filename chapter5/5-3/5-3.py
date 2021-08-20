18e2d193de2b0ac878c26e0674f72256f43ff335
adarke <47354567+adarke@users.noreply.github.com>
2021-08-20 23:59:15 +0100

    5-3: exercise complete

---
+++ chapter5/5-3/5-3.py
@@ -0,0 +1,15 @@
+""" 5-3 -- Dictionary.
+
+Create a dictionary that contains different attributes about you: height,
+favourite color, favourite author, etc.
+"""
+
+
+me = {
+    "height": 180,
+    "age": 25,
+    "favourite colour": "green",
+    "favourite author": "Haruki Murakami",
+    }
+
+print(me)
