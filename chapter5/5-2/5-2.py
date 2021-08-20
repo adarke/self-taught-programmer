3a10d021a8da54c5e64a0484bec454deda55d3c2
adarke <47354567+adarke@users.noreply.github.com>
2021-08-20 23:59:02 +0100

    5-2: exercise complete

---
+++ chapter5/5-2/5-2.py
@@ -0,0 +1,16 @@
+""" 5-2 -- List of Tuples.
+
+Create a list of tuples, with each tuple containing the longitude and latitude
+of somewhere you've lived or visited. 
+"""
+
+
+locations = [
+    (35.6762, 139.6503),
+    (70.5308, 23.5717),
+    (18.7669, 46.8691),
+    (0.9538, 90.9656),
+    ]
+    
+print(locations)
+             
