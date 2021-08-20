b9bf4fab16a70a59dc530e3d9c54c2b40021f55c
adarke <47354567+adarke@users.noreply.github.com>
2021-08-20 23:56:18 +0100

    3-2: exercise complete

---
+++ chapter3/3-2/3-2.py
@@ -0,0 +1,13 @@
+""" 3-2 -- Conditional Print.
+
+Write a program that prints a message if a variable is less than 10, and
+different message if the variable is greater than or equal to 10.
+"""
+
+
+value = 5
+
+if value < 10:
+    print("value is less than ten")
+else:
+    print("value is greater than or equal to ten")
