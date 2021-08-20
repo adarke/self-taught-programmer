3121bc1239b252a34ec10e201def22fd02bc8dba
adarke <47354567+adarke@users.noreply.github.com>
2021-08-21 00:04:12 +0100

    6-1: exercise complete

---
+++ chapter6/6-1/6-1.py
@@ -0,0 +1,13 @@
+""" 6-1 -- Every Character.
+
+Print every character in the string "Camus".
+"""
+
+
+author = "Camus"
+
+print(author[0])
+print(author[1])
+print(author[2])
+print(author[3])
+print(author[4])
