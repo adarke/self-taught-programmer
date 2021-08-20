988239cf8776f900152db5909b5ea4afcc24abc1
adarke <47354567+adarke@users.noreply.github.com>
2021-08-21 00:05:18 +0100

    6-6: exercise complete

---
+++ chapter6/6-6/6-6.py
@@ -0,0 +1,8 @@
+""" 6-6 -- Character Replace. 
+
+Replace every instance of "s" in "A screaming comes across the sky." with a 
+dollar sign.
+"""
+
+
+print("A screaming comes across the sky.".replace("s", "$"))
