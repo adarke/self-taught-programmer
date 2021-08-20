23fcf515453b710223e19e8c472ab1e479121d10
adarke <47354567+adarke@users.noreply.github.com>
2021-08-20 23:57:33 +0100

    3-6: exercise complete

---
+++ chapter3/3-6/3-6.py
@@ -0,0 +1,15 @@
+""" 3-6 -- Age String.
+
+Write a program with a variable age assigned to an integer that prints
+different strings depending on what integer age is.
+"""
+
+
+age = 17
+
+if age <= 12:
+    print("child")
+elif age <= 19:
+    print("teenager")
+else:
+    print("adult")
