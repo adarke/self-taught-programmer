cebbd16a05537fdf307f23918530da5e94aadef0
adarke <47354567+adarke@users.noreply.github.com>
2021-08-21 00:12:53 +0100

    16-2: exercise complete

---
+++ chapter16/16-2/16-2.sh
@@ -0,0 +1,18 @@
+### 16-2 -- Directory Navigation.
+#
+# Navigate to your home directory from another directory using an absolute and 
+# relative path.
+#
+###
+
+
+#!/bin/bash
+
+cd /
+pwd
+cd /Users/$USER
+pwd
+cd /
+pwd
+cd Users/$USER
+pwd
