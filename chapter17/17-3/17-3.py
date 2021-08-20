670baab15c950488e216cbbb188cc8a1ed08e712
adarke <47354567+adarke@users.noreply.github.com>
2021-08-21 00:13:42 +0100

    17-3: exercise complete

---
+++ chapter17/17-3/17-3.py
@@ -0,0 +1,14 @@
+""" 17-3 -- Wildcard Regex.
+
+Create a regular expression that matches any word that starts with any character
+and is followed by two o's. Then use Python's re module to match boo and loo in 
+the sentence The ghost that says boo haunts the loo.
+"""
+
+
+import re
+
+line = "The ghost that says boo haunts the loo."
+matches = re.findall(".oo", line)
+
+print(matches)
