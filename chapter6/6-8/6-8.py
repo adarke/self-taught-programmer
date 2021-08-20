240d62009cebcd6cca683e6a09c32689ddf09382
adarke <47354567+adarke@users.noreply.github.com>
2021-08-21 00:05:58 +0100

    6-8: exercise complete

---
+++ chapter6/6-8/6-8.py
@@ -0,0 +1,10 @@
+""" 6-8 -- Print Dialogue.
+
+Find dialogue in your favorite book (containing quotes) and turn it into a 
+string.
+"""
+
+
+dialogue = 'Use a method to find the first index of the character "m" in the \
+string "Hemingway".'
+print(dialogue)
