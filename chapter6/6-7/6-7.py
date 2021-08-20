859fede4799ff99152a32333b467ddc0ab467d33
adarke <47354567+adarke@users.noreply.github.com>
2021-08-21 00:05:49 +0100

    6-7: exercise complete

---
+++ chapter6/6-7/6-7.py
@@ -0,0 +1,11 @@
+""" 6-7 -- First Index.
+
+Use a method to find the first index of the character "m" in the string 
+"Hemingway".
+"""
+
+
+try:
+    print("Hemingway".index("m"))
+except:
+    print("Not found.")
