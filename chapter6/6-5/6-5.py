9c85beb8f24ca7778216b31494869c0514f160d2
adarke <47354567+adarke@users.noreply.github.com>
2021-08-21 00:04:55 +0100

    6-5: exercise complete

---
+++ chapter6/6-5/6-5.py
@@ -0,0 +1,12 @@
+""" 6-5 -- Join List.
+
+Take the list ["The", "fox", "jumped", "over", "the", "fence", "."] and turn it 
+into a grammatically correct string. There should be a space between each word, 
+but no space between the word fence and the period that follows it. (Don't 
+forget, you learned a method that turns a list of strings into a single string.)
+"""
+
+
+list = ["The", "fox", "jumped", "over", "the", "fence", "."]
+
+print(" ".join(list).replace(" .", "."))
