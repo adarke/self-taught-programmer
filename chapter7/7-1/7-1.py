54baf85f18e1b175a2e818f3d58ff9a9ee96e642
adarke <47354567+adarke@users.noreply.github.com>
2021-08-21 00:06:33 +0100

    7-1: exercise complete

---
+++ chapter7/7-1/7-1.py
@@ -0,0 +1,14 @@
+""" 7-1 -- Print Each Item In List.
+
+Print each item in the following list: ["The Walking Dead", "Entourage", "The 
+Sopranos", "The Vampire Diaries"].
+"""
+
+
+shows = ["The Walking Dead",
+         "Entourage",
+         "The Sopranos",
+         "The Vampire Diaries"]
+
+for i in shows:
+    print(i)
