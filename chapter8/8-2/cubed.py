b6f3d0a6742e76c918705f663f005ef78c7ba679
adarke <47354567+adarke@users.noreply.github.com>
2021-08-21 00:09:01 +0100

    8-2: added cubed.py

---
+++ chapter8/8-2/cubed.py
@@ -0,0 +1,17 @@
+""" cubed -- Cubed Module.
+
+Create a module named cubed with a function that takes a number as a parameter, 
+and returns the number cubed. Import and call the function from another module.
+"""
+
+
+def cubed(x):
+    """ Calculate the cube of the argument.
+
+    Args:
+        x: The number to be cubed.
+
+    Returns:
+        The value of x cubed.
+    """
+    return x ** 3
